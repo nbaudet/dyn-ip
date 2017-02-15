bc8defba09bbd3207e3f7f7ec59ca79eb47045fb
Nicolas BAUDET <nb@dl.local>
2017-02-15 13:51:24 +0100

    First try with jsftp

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dyn-ip
+## What this package does
 Let this soft run on any machine on your local network, and it will update a file on your web server with your public IP through FTP.
+It is useful when you have a dynamic IP address AND a webserver and don't want to use a dynDNS client.
+
+## Installation
+Prerequisite: you need to have Node.js and Git installed on your machine.
+
+```bash
+git clone https://github.com/nbaudet/dyn-ip.git
+cd dyn-ip
+npm install
+...
+```
+1. On your FTP server, create a folder where the files should be served, i.e. "dyn-ip".
+2. Rename or copy `config.example.yml` to `config.yml` and set the correct FTP values. Set the path to the where you want the files to be uploaded, i.e. "dyn-ip". **Be careful not to overwrite an existing index.html file** by giving an existing path.
+
+## Launch as a deamon
+...
+
+## Start at boot-time on a Raspberry Pi
+...
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 yaml = require('js-yaml');
 fs = require('fs');
 const publicIp = require('public-ip');
+var JSFtp = require("jsftp");
 
 // Get config Yaml content, or throw exception on error
 function readYaml(fileName) {
@@ -12,11 +13,60 @@ function readYaml(fileName) {
     }
 }
 
-// Save current config to file
+// Save content to yaml file
 function writeYaml(fileName, content) {
-    fileName = fileName || 'config.yml';
     try {
-        fs.writeFileSync(fileName, yaml.safeDump(content)) //.safeLoad(fs.readFileSync(fileName, 'utf8'));
+        fs.writeFileSync(fileName, yaml.safeDump(content))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// Save content to json file
+function writeJson(fileName, content) {
+    try {
+        fs.writeFileSync(fileName, JSON.stringify(content), 'utf8')
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// Upload a single file to the server
+function uploadSingleFile(ftp, sourcePath, destPath, callback) {
+    ftp.put(sourcePath, destPath, function(hadError) {
+        if (!hadError) {
+            console.log("Successfully uploaded " + sourcePath + " to " + destPath);
+        }
+        if (callback) callback(ftp);
+    });
+}
+
+// Uploads the files to the server
+function uploadFiles(config) {
+    try {
+        let ftp = new JSFtp({
+            host: config.server.host,
+            //port: 3331, // defaults to 21
+            user: config.server.username, // defaults to "anonymous"
+            pass: config.server.password, // defaults to "@anonymous"
+            debugMode: true
+        });
+        ftp.on('progress', function(progress) {
+            console.log(progress);
+        })
+        ftp.on('jsftp_debug', function(eventType, data) {
+            console.log('DEBUG: ', eventType);
+            console.log(JSON.stringify(data, null, 2));
+        });
+
+        // Tests existence of destination folder and creates it if needed
+        // ...
+
+        // Uploads the file
+        uploadSingleFile(ftp, "pub/index.html", config.server.path + "/index.html",
+            uploadSingleFile(ftp, "pub/redirect.json", config.server.path + "/redirect.json")
+        );
+
     } catch (e) {
         console.log(e);
     }
@@ -28,11 +78,36 @@ function main() {
     // Get the public IP
     publicIp.v4({ https: true }).then(ip => {
         console.log(ip);
-        if (ip != config.currentIp) {
+
+        let pubConfig = {
+            redirectIp: ip,
+            oldIps: []
+        };
+
+        writeJson("pub/redirect.json", pubConfig);
+
+        // If there is a new IP...
+        /*if (ip != config.currentIp) {
             console.log("Writing new IP to config file.");
             config.currentIp = ip;
+            // Updates the config files (local and public)
             writeYaml('config.yml', config);
-        }
+            // ... public file*/
+
+        // Updates the redirector
+        //uploadFiles(config);
+
+        let ftp = new JSFtp({
+            host: config.server.host,
+            port: 21,
+            user: config.server.username, // defaults to "anonymous"
+            pass: config.server.password, // defaults to "@anonymous"
+            debugMode: true
+        });
+
+        uploadSingleFile(ftp, "pub/index.html", config.server.path + "/index.html");
+        uploadSingleFile(ftp, "pub/redirect.json", config.server.path + "/redirect.json")
+        //}
     });
 }
 
